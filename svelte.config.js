776398767e234500b9bea26c7e32e9347921cfc5
Charlie <ickyicky@gmail.com>
2022-06-27 16:05:11 +0900

    린트 에러 수정

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node"
+import adapter from "@sveltejs/adapter-node";
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
   kit: {
     adapter: adapter({
-      out: 'build'
+      out: 'build',
     }),
   },
 };
