654efe7bd755d1dc2964fc301107156bfc639000
dj <dj@gollala.com>
2022-06-30 14:17:04 +0900

    lint 변경 및 graphql api 추가

--- src/routes/api/graphql/_graphql.ts
+++ src/routes/api/graphql/_graphql.ts
@@ -1,45 +1,27 @@
 import { buildSchema, graphql } from 'graphql';
-import { list, product } from '../product/_product';
-/*import { readFileSync } from 'fs';
-const contentsJson = readFileSync('./content.json', 'utf8');*/
-import contentsJson from '../../../../static/data/content.json';
-console.log(contentsJson);
+import contentJson from '../../../../static/data/content.json';
+import productJson from '../../../../static/data/product.json';
+import celebJson from '../../../../static/data/celeb.json';
+import exposedJson from '../../../../static/data/exposed.json';
+import programJson from '../../../../static/data/program.json';
+import relationJson from '../../../../static/data/relation.json';
 
 export async function Graphql(query: string) {
   const schema = buildSchema(`
-		type Wholesale {
-            wholesaleStoreId: ID!
-			address: String
-            building: String
-            favorite: Boolean
-            floor: String
-            formattedPhone: String
-            imgPath: String
+		type Product {
+		    id: String!
             name: String!
-            phone: String
-            section: String
-            wsSeq: String
+            brand: String
+            price: Int,
+            discount: Int,
+            category: String,
+            exposed: [[Int]]
 		}
 		
-		type Category {
-            gender: String
-            itemType: String
-            itemLarge: String
-            itemMiddle: String
-        }
-		
-		type Product {
-			productId: ID!
-            code: String!
-            createDt: Float!
-            expired: Boolean
-            imgPaths: [String]
-            item: Category
-            name: String!
-            price: Int!
-            updateDt: Float
-            wholesale: Wholesale
-            similar: [Product]
+        type Program {
+          id: String!
+          name: String!
+          description: String
 		}
 		
 		type Content {
@@ -51,28 +33,125 @@ export async function Graphql(query: string) {
           round: Int
           description: String
           url: String
+          videoId: String
           thumb: String
+          program: Program
 		}
+		
+		type Celeb {
+          id: String!
+          name: String!
+          description: String
+          category: String
+		}
+		
+	
 		
 		type Query {
 			products: [Product]
-			product(id:ID): Product
+			product(id:ID!): Product
 			contents: [Content]
+			content(id:ID!): Content
+			celebs: [Celeb]
+			celeb(id:ID!): Celeb
+			programs: [Program]
+			program(id:ID!): Program
+			getProductsByContentId(id:ID!): [Product]
+			getCelebsByContentId(id:ID!): [Celeb]
+            getContentsByProgramId(id:ID!): [Content]
+            getContentsByProductId(id:ID!): [Content]
+            getCelebsByProductId(id:ID!): [Celeb]
+            getProductByCelebId(id:ID!): [Product]
+            getContentsByCelebId(id:ID!): [Content]
+            getProductsByCategory(category:String!): [Product]
   	    }
 	`);
 
   const rootValue = {
-    products: async () => {
-      const result = await list();
-      return result;
+    products: () => {
+      return productJson;
     },
-    product: async ({ id }: {id:string}) => {
-      const result = await product(id);
-      return result;
+    product: ({ id }: {id:string}) => {
+      return productJson.find(product => product.id === id);
     },
     contents: () => {
-      return contentsJson;
-    }
+      const result = contentJson.map(content => {
+        return {
+          ...content,
+          program: programJson.find(program => program.id === content.programId),
+        };
+      });
+      console.log(result);
+
+      return result;
+    },
+    content: ({ id }: {id:string}) => {
+      const content: any = contentJson.find(content => content.id === id);
+      return {
+        ...content,
+        program: programJson.find(program => program.id === content.programId),
+      };
+    },
+    celebs: () => {
+      return celebJson;
+    },
+    celeb: ({ id }: {id:string}) => {
+      return celebJson.find(celeb => celeb.id === id);
+    },
+    programs: () => {
+      return programJson;
+    },
+    program: ({ id }: {id:string}) => {
+      return programJson.find(program => program.id === id);
+    },
+    getProductsByContentId: ({ id }: {id:string}) => {
+      const products = exposedJson
+        .filter(exposed => exposed.content === id)
+        .map(exposed => {
+          const product = productJson.find(product => product.id === exposed.product);
+          return {
+            ...product,
+            exposed: exposed.timelines,
+          };
+        });
+      return products;
+    },
+    getCelebsByContentId: ({ id }: {id:string}) => {
+      const celebIds = relationJson
+        .filter(relation => relation.content === id)
+        .map(relation => relation.celeb);
+      return celebJson.filter(celeb => celebIds.includes(celeb.id));
+    },
+    getContentsByProductId: ({ id }: {id:string}) => {
+      const contentIds = relationJson
+        .filter(relation => relation.product === id)
+        .map(relation => relation.content);
+      return contentJson.filter(content => contentIds.includes(content.id));
+    },
+    getCelebsByProductId: ({ id }: {id:string}) => {
+      const celebIds = relationJson
+        .filter(relation => relation.product === id)
+        .map(relation => relation.celeb);
+      return celebJson.filter(celeb => celebIds.includes(celeb.id));
+    },
+    getProductByCelebId: ({ id }: {id:string}) => {
+      const productIds = relationJson
+        .filter(relation => relation.celeb === id)
+        .map(relation => relation.product);
+      return productJson.filter(product => productIds.includes(product.id));
+    },
+    getContentsByCelebId: ({ id }: {id:string}) => {
+      const contentIds = relationJson
+        .filter(relation => relation.celeb === id)
+        .map(relation => relation.content);
+      return contentJson.filter(content => contentIds.includes(content.id));
+    },
+    getContentsByProgramId: ({ id }: {id:string}) => {
+      return contentJson.filter(content => content.programId === id);
+    },
+    getProductsByCategory: ({ category }: {category:string}) => {
+      return productJson.filter(product => product.category === category);
+    },
   };
 
   return graphql({
--- src/routes/api/graphql/index.ts
+++ src/routes/api/graphql/index.ts
@@ -1,15 +1,15 @@
 import { Graphql } from './_graphql';
-import type {RequestHandler} from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
 
 /**
  * graphql query 를 이용하여 데이터를 리턴
  * @param {Request} request
  * @returns {Promise<{body: any}>}
  */
-export const post:RequestHandler = async ({request}) => {
+export const post:RequestHandler = async ({ request }) => {
   const query = await request.json(); // or .json(), or .text(), etc
   const result: any = await Graphql(query.query);
   return {
-    body: result
+    body: result,
   };
-}
+};
