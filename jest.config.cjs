ac79a0a61ebb02c870e62d06f9233ff265a73089
Charlie <ickyicky@gmail.com>
2022-06-30 16:32:20 +0900

    jest-context 추가

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,25 +1,31 @@
 module.exports = {
-    jest: {
-        verbose: true
-    },
-    transform: {
-        "^.+\\.svelte$": [
-            "svelte-jester",
-            {
-                preprocess: "./svelte.config.test.cjs",
-            },
-        ],
-        "^.+\\.ts$": "ts-jest",
-        "^.+\\.js$": "ts-jest",
-    },
-    moduleFileExtensions: ["js", "ts", "svelte"],
-    moduleNameMapper: {
-        "^\\$lib(.*)$": "<rootDir>/src/lib$1",
-        "^\\$app(.*)$": [
-            "<rootDir>/.svelte-kit/dev/runtime/app$1",
-            "<rootDir>/.svelte-kit/build/runtime/app$1",
-        ],
-    },
-    setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
-    testEnvironment: "jsdom",
+  jest: {
+    verbose: true
+  },
+  transform: {
+    "^.+\\.svelte$": [
+      "svelte-jester",
+      {
+        preprocess: "./svelte.config.test.cjs",
+      },
+    ],
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "ts-jest",
+  },
+  moduleFileExtensions: ["js", "ts", "svelte"],
+  moduleNameMapper: {
+    "^\\$lib(.*)$": "<rootDir>/src/lib$1",
+    "^\\$app(.*)$": [
+      "<rootDir>/.svelte-kit/dev/runtime/app$1",
+      "<rootDir>/.svelte-kit/build/runtime/app$1",
+    ],
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/jest-setup.ts",
+    'jest-plugin-context/setup',
+  ],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
 };
