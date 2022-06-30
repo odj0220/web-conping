1a66283b85bfa1f967092e567dc9a5fd9285df9c
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-07-01 01:49:32 +0900

    임시 캐러셀 예제 생성

---
+++ src/routes/me/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Hscroller from '$component/Hscroller.svelte';
+    import ShortList from "../../components/ShortList.svelte";
+</script>
+
+<Hscroller>
+    <ShortList></ShortList>
+</Hscroller>
+
+<style lang="scss">
+
+</style>
