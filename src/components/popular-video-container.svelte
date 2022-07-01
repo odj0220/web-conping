babcd1757424f8d309e61d1611286f0c7fa4a9a7
Charlie <ickyicky@gmail.com>
2022-07-01 16:49:10 +0900

    지금 인기있는 영상 추가

---
+++ src/components/popular-video-container.svelte
@@ -0,0 +1,8 @@
+<script>
+  import contents from '../fixtures/contents';
+
+  import PopularVideos from './popular-videos.svelte';
+
+</script>
+
+<PopularVideos {contents}></PopularVideos>
---
+++ src/components/popular-videos.spec.ts
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/svelte';
+
+import PopularVideos from './popular-videos.svelte';
+
+import contents from '../fixtures/contents';
+
+describe('popular-videos', () => {
+  const renderVideos = () => {
+    return render(PopularVideos, {
+      props: { contents },
+    });
+  };
+
+  it('renders title', () => {
+    const { container } = renderVideos();
+
+    expect(container).toHaveTextContent('지금 인기있는 영상');
+  });
+
+  context('with contents', () => {
+    it('renders contents name text', () => {
+      const { container } = renderVideos();
+
+      contents.forEach(({ program }) => {
+        expect(container).toHaveTextContent(program.name);
+      });
+    });
+  });
+});
