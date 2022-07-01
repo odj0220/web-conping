2079421c6dcf715389d1bc9d3eecba0aea809078
Charlie <ickyicky@gmail.com>
2022-07-01 17:09:05 +0900

    store 파일 수정

--- src/routes/stores/index.svelte
+++ src/routes/stores/index.svelte
@@ -1,16 +1,3 @@
-<script>
-  import products from '../../fixtures/products'; //TODO: 목데이터
-</script>
-
 <h1>
   관련 상품
 </h1>
-
-<ul>
-  {#each products as product }
-    <li>
-      <img src={product.imageUrl} alt={product.title}>
-      {product.title}
-    </li>
-  {/each}
-</ul>
