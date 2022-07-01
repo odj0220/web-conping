6c843894d5ec8c2abde590206433d21369f1c587
cheonyulin@gmail.com <mason@gollala.com>
2022-07-01 17:04:27 +0900

    파스칼 케이스로 이름 변경

---
+++ src/components/MostHighLightContainer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Hscroller from './Hscroller.svelte';
+    import ShortList from "./ShortList.svelte";
+
+</script>
+
+<header>
+    <h1>많이 본 하이라이트</h1>
+</header>
+
+<Hscroller>
+    <ShortList></ShortList>
+</Hscroller>
+
+<style lang="scss">
+    header {
+     margin-bottom: 1.2rem;
+    }
+</style>
