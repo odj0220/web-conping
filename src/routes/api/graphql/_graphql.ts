1f7d5abaef0ea7921099ae05149745ef608a18ef
dj <dj@gollala.com>
2022-07-04 18:28:53 +0900

    graphql 스키마 작업중

--- src/routes/api/graphql/_graphql.ts
+++ src/routes/api/graphql/_graphql.ts
@@ -5,52 +5,74 @@ import celebJson from '../../../../static/data/celeb.json';
 import exposedJson from '../../../../static/data/exposed.json';
 import programJson from '../../../../static/data/program.json';
 import relationJson from '../../../../static/data/relation.json';
+import watchingJson from '../../../../static/data/watching.json';
+import type { GraphQLSchema } from 'graphql/type/schema';
 
 export async function Graphql(query: string) {
-  const schema = buildSchema(`
+  const schema: GraphQLSchema = buildSchema(`
 		type Product {
-		    id: String!
-            name: String!
-            brand: String
-            price: Int
-            discount: Int
-            category: String
-            exposed: [[Int]]
+      id: String!
+      name: String!
+      brand: String
+      price: Int
+      discount: Int
+      category: String
+      exposed: [[Int]]
 		}
 		
-        type Program {
-          id: String!
-          name: String!
-          description: String
+    type Program {
+      id: String!
+      name: String!
+      description: String,
+      bannerImg: String
+      profileImg: String
+      releasedAt: Float
+      airingAt: Float
+      airingBeginAt: Float
+      airingEndAt: Float 
 		}
 		
 		type Content {
 		  id: ID!
-          name: String!
-          programId: String
-          contentType: String
-          createDt: Float!
-          round: Int
-          description: String
-          url: String
-          videoId: String
-          thumb: String
-          program: Program
+      name: String!
+      programId: String
+      contentType: ContentType
+      createDt: Float!
+      round: Int
+      description: String
+      url: String
+      videoId: String
+      thumb: String
+      program: Program
+      currentTime: Float
 		}
 		
 		type Celeb {
-          id: String!
-          name: String!
-          description: String
-          category: String
+      id: String!
+      name: String!
+      description: String
+      category: String
+		}
+		
+		enum Order {
+		  LIKE
+		  VIEW
+		  CREATE
+		}
+		
+		enum ContentType {
+		  EPISODE
+		  HIGHLIGHT
+		  SHORTS
+		  ORIGIN
 		}
 		
 	
 		
 		type Query {
-			products: [Product]
+		  products: [Product]
 			product(id:ID!): Product
-			contents: [Content]
+			contents(order: Order, type: ContentType): [Content]
 			content(id:ID!): Content
 			celebs: [Celeb]
 			celeb(id:ID!): Celeb
@@ -58,14 +80,16 @@ export async function Graphql(query: string) {
 			program(id:ID!): Program
 			getProductsByContentId(id:ID!): [Product]
 			getCelebsByContentId(id:ID!): [Celeb]
-            getContentsByProgramId(id:ID!): [Content]
-            getContentsByProductId(id:ID!): [Content]
-            getCelebsByProductId(id:ID!): [Celeb]
-            getProductByCelebId(id:ID!): [Product]
-            getContentsByCelebId(id:ID!): [Content]
-            getProductsByCategory(category:String!): [Product]
-  	    }
+      getContentsByProgramId(id:ID!): [Content]
+      getContentsByProductId(id:ID!): [Content]
+      getCelebsByProductId(id:ID!): [Celeb]
+      getProductByCelebId(id:ID!): [Product]
+      getContentsByCelebId(id:ID!): [Content]
+      getProductsByCategory(category:String!): [Product]
+      continueWatching: [Content]
+    }
 	`);
+
 
   const rootValue = {
     products: () => {
@@ -74,22 +98,24 @@ export async function Graphql(query: string) {
     product: ({ id }: {id:string}) => {
       return productJson.find(product => product.id === id);
     },
-    contents: () => {
+    contents: ({ order, type }: {order: string, type: string}) => {
+      console.log(order, type, schema.getType('Order'));
       const result = contentJson.map(content => {
         return {
           ...content,
           program: programJson.find(program => program.id === content.programId),
         };
       });
-      console.log(result);
-
       return result;
     },
     content: ({ id }: {id:string}) => {
       const content: any = contentJson.find(content => content.id === id);
+      const program = programJson.find(program => program.id === content.programId);
+      const currentTime = (watchingJson.find(watching => watching.contentId === content.id))?.currentTime || 0;
       return {
         ...content,
-        program: programJson.find(program => program.id === content.programId),
+        program,
+        currentTime,
       };
     },
     celebs: () => {
@@ -151,6 +177,17 @@ export async function Graphql(query: string) {
     },
     getProductsByCategory: ({ category }: {category:string}) => {
       return productJson.filter(product => product.category === category);
+    },
+    continueWatching: () => {
+      return contentJson
+        .filter(content => watchingJson.map(watching => watching.contentId).includes(content.id))
+        .map(content => {
+          const currentTime = (watchingJson.find(watching => watching.contentId === content.id))?.currentTime;
+          return {
+            ...content,
+            currentTime,
+          };
+        });
     },
   };
 
