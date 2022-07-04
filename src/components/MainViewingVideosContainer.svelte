ebc84fadccdb02e1afa247b80656ba794ba12927
cheonyulin@gmail.com <mason@gollala.com>
2022-07-04 15:05:26 +0900

    create main-viewing-videos-container

---
+++ src/components/MainViewingVideosContainer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import Hscroller from "./HorizontalScroller.svelte";
+    import {onMount} from "svelte";
+
+    let contents: any[] = [];
+</script>
+
+<section>
+    <header>
+        <h1>시청중인 영상</h1>
+    </header>
+
+    <main>
+        <Hscroller>
+            <section>anksflnfkfkakf</section>
+        </Hscroller>
+    </main>
+</section>
+
+
+<style lang="scss">
+
+</style>
