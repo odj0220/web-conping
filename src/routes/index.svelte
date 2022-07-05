4ee435c2752c7ebbc0ea95df5d058300d9ab7d34
cheonyulin@gmail.com <mason@gollala.com>
2022-07-05 13:58:27 +0900

    apply containers to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,9 @@
 <script>
   import PopularVideoContainer from '../components/PopularVideoContainer.svelte';
-
+  import MainViewingVideosContainer from '../components/MainViewingVideosContainer.svelte';
+  import MainShortsContainer from '../components/MainShortsContainer.svelte';
 </script>
 
+<MainViewingVideosContainer/>
+<MainShortsContainer/>
 <PopularVideoContainer />
