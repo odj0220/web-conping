851beac08a8e467736c5654a88ec954c5ea0e1c9
cheonyulin@gmail.com <mason@gollala.com>
2022-07-05 16:51:06 +0900

    publish banner and container

---
+++ src/components/Banners.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+    import { Swiper, SwiperSlide } from 'swiper/svelte';
+    import SwiperCore, { Autoplay, Pagination } from 'swiper';
+    import 'swiper/css';
+    import 'swiper/css/pagination';
+
+    const banners: any[] = [
+      {
+        src: '/images/thumb.png',
+        title: 'Banner 01',
+      },
+      {
+        src: '/images/thumb.png',
+        title: 'Banner 02',
+      },
+      {
+        src: '/images/thumb.png',
+        title: 'Banner 02',
+      },
+      {
+        src: '/images/thumb.png',
+        title: 'Banner 02',
+      },
+    ];
+
+    SwiperCore.use([Pagination, Autoplay]);
+
+    function handleClick(swiper, event) {
+      console.log('Real Index', swiper.detail[0].realIndex);
+    }
+</script>
+
+<section class="swiper-container">
+    <Swiper
+            pagination={{ clickable: true }}
+            spaceBetween={0}
+            loop={true}
+            autoplay={{ deplay: 1000 }}
+            elementClass="main-banner"
+            slideToClickedSlide={true}
+            on:click={handleClick}
+    >
+        {#each banners as banner}
+            <SwiperSlide>
+                <section class="banner-wrapper">
+                    <img src={banner.src} alt="">
+                </section>
+            </SwiperSlide>
+        {/each}
+    </Swiper>
+</section>
+
+<style lang="scss">
+    .swiper-container {
+      padding: 0 1.6rem;
+
+      :global(.swiper-pagination ) {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        left: -1rem;
+      }
+
+      :global(.swiper-pagination-bullet) {
+        width: 0.6rem;
+        height: 0.6rem;
+        background: #fff;
+        border: 1px solid pink;
+        opacity: 0.5;
+      }
+      :global(.swiper-pagination-bullet-active) {
+        width: 2rem;
+        transition: width .5s;
+        border-radius: 0.3rem;
+        background: #fff;
+        border: 1px solid transparent;
+        opacity: 1;
+      }
+
+      .banner-wrapper {
+        width: 100%;
+        padding-bottom: 100%;
+        position: relative;
+
+        img {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+</style>
---
+++ src/components/MainBannerContainer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount, SvelteComponent } from 'svelte';
+  import MainBannerContainer from './MainBannerContainer.svelte';
+
+  let Banners: SvelteComponent;
+
+  onMount(async () => {
+    const module = await import('./Banners.svelte');
+    Banners = module.default;
+  });
+</script>
+
+<h1>Main Banner Container</h1>
+<section>
+    <svelte:component this={Banners}></svelte:component>
+</section>
+
+<style lang="scss">
+</style>
