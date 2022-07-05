19a6c5ddd31bb586eceab17dbad014ad6acc13a5
Charlie <ickyicky@gmail.com>
2022-07-05 17:09:32 +0900

    코드정리

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -11,14 +11,7 @@
 
 <style lang="scss">
   section {
-    // background-color: #212121;
-    // border-radius: 0.4rem;
-    // margin: 0 1.6rem;
-    // padding: 1.6rem 1.2rem 2.6rem 1.2rem;
-    // margin-bottom: 4rem;
-
     header {
-      // margin-bottom: 1.6rem;
       h1 {
         font-size: 1.6rem;
         line-height: 1.997rem;
