e6dd7bdf4d3300ac64862573518d0efb57c771a9
cheonyulin@gmail.com <mason@gollala.com>
2022-07-06 11:40:16 +0900

    화면에서 두 개만 보여주고 있으므로, 목 데이터에서의 배열 길이를 2만큼 짤라서 내려주도록 바꿈

--- src/components/PopularVideoContainer.svelte
+++ src/components/PopularVideoContainer.svelte
@@ -1,9 +1,9 @@
 <script>
 import CenterSection from '../styles/CenterSection.svelte';
-
 import contents from '../fixtures/contents';
-
 import Videos from './Videos.svelte';
+
+const popularVideos = contents.splice(0, 2);
 
 const handleClick = (id) => {
   //TODO: click 이벤트
@@ -13,7 +13,7 @@ const handleClick = (id) => {
 
 <CenterSection>
   <Videos
-    {contents}
+    contents={popularVideos}
     title="지금 인기있는 영상"
     onClick={handleClick}
   />
