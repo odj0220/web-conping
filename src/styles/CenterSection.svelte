3215658e3e6111dcc9b9e027364e6ce1bd4fff70
Charlie <ickyicky@gmail.com>
2022-07-06 16:36:35 +0900

    section 스타일 변경

--- src/styles/CenterSection.svelte
+++ src/styles/CenterSection.svelte
@@ -1,16 +1,26 @@
-<section>
+<script lang="ts">
+  export let type: string;
+</script>
+
+<section class={type}>
   <slot />
 </section>
 
 <style lang="scss">
   @import '../styles/modules.scss';
   @import '../styles/variables.scss';
+  
   section {
-    margin: 4rem 1.6rem;
-    padding: 1.6rem;
+    margin: 0 1.6rem;
+    padding: 1.6rem 0;
     display: flex;
     flex-direction: column;
     gap: 1.6rem;
+    border-radius: 0.4rem;
     background-color: $bg-black-21;
+    
+    &.transparency {
+      background-color: transparent;
+    }
   }
 </style>
