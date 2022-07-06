d59deb7301b95a8210fa95ee7c9c9e0f8e1c6909
cheonyulin@gmail.com <mason@gollala.com>
2022-07-06 16:59:53 +0900

    modify contents img to youtube iframe

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,84 +1,215 @@
-<script>
-  export let onClick;
-  export let content;
-</script>
+<script type="ts">
+  import { guid, toHHMMSS } from '$lib/util';
+  export { toHHMMSS } from '$lib/util';
+  import { onMount } from 'svelte';
+  import YP from 'youtube-player';
+  import type { YouTubePlayer } from 'youtube-player/dist/types';
 
-<li
-  class="item-card"
-  role="list"
->
-  <div class="image-wrapper">
-    <img
-      src={content.thumb} alt={content.name}
-      on:click={() => onClick(content.id)}
-    >
-  </div>
-  <div class="content-info-wrapper">
-    <div class="info-title">
-      {(content.program.name).repeat(10)}
-      <!-- TODO: 테스트용 코드 수정 -->
-    </div>
-    <div class="sub-info">
-      <img class="profile-icon" src={content.thumb} alt="방송로고">
-      <span class="profile-info">랜선뷰티 &middot; 5화 &middot; 2일 전</span>
-      <!-- TODO: 하드코딩 -->
-    </div>
-  </div>
-</li>
+  export let content: any;
+  export let onClick: () => void;
 
-<style lang="scss">
-  @import '../styles/modules.scss';
-  @import '../styles/variables.scss';
+  let playTime;
+  let displayLogo = true;
+  const playerId = guid();
 
-  .item-card {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    border-radius: 0.4rem;
-    overflow: hidden;
-    
-    .image-wrapper {
-      width: 100%;
-      height: 56.4%;
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
+  let player: YouTubePlayer;
+  let firstLoad = true;
+  let displayLogDebounce;
+  let pauseTimer: any = null;
 
-      cursor: pointer;
-    }
+  const PLAYER_STATE = {
+    NOT_STARTED: -1,
+    ENDED: 0,
+    PLAYING: 1,
+    PAUSED: 2,
+    BUFFERING: 3,
+  };
 
-    .content-info-wrapper {
-      display: flex;
-      flex-direction: column;
-      gap: 0.8rem;
-      padding: 1.2rem;
-      background-color: $bg-black-21;
+  function loadYoutubePlayer() {
+    const playerVars = {
+      controls: 0, //플레이어 컨드롤러 표시여부
+      rel: 0, //연관동영상 표시여부
+      mute: 1,
+      playsinline: 1, //ios환경에서 전체화면으로 재생하지 않게하는 옵션
+      autoplay: 1, //자동재생 여부(모바일에서 작동하지 않습니다. mute설정을 하면 작동합니다.)
+      loop: 1,
+      modestbranding: 1,
+      disablekb: 1,
+      enablejsapi: 1,
+    };
 
-      .info-title {
-        @include ellipsis(3);
-        @include caption1-400;
-      }
+    const option = {
+      videoId: content.videoId,
+      playerVars,
+    };
 
-      .sub-info {
-        display: flex;
-        flex-direction: row;
-        gap: 0.8rem;
-        align-items: center;
-        
-        .profile-icon {
-          width: 2.4rem;
-          height: 2.4rem;
-          overflow: visible;
-          border-radius: 1.2rem;
-          object-fit: fill;
-        }
-        .profile-info {
-          @include caption2-400;
-        }
-      }
-    }
+    player = YP(playerId, option);
   }
 
+  function setIntersectionObserver(element: HTMLElement) {
+    const io = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          play();
+        } else {
+          // 최초 로딩시 검은화면을 방지하기 위해 1초뒤 일시정지
+          pause();
+        }
+      });
+    }, {
+      threshold: 1,
+    });
+    io.observe(element);
+  }
+
+  function onPlayerReady() {
+    player.on('ready', (event) => {
+      const target = event.target;
+      const element = target.i;
+  
+      setIntersectionObserver(element);
+    });
+  }
+
+  function onPlayerStateChange() {
+    player.on('stateChange', event => {
+      const status = event.data;
+
+      if (status === PLAYER_STATE.NOT_STARTED) {
+        if (firstLoad) {
+          firstLoad = false;
+        }
+        return;
+      }
+
+      if (status === PLAYER_STATE.PLAYING) {
+        console.log('시작');
+        /*
+        * 시작할때 로고를 가려야한다.
+        * */
+        // displayLogDebounce = setTimeout(() => {
+        //   displayLogo = false;
+        //   firstLoad = false;
+        // }, firstLoad ? 3200 : 100);
+        return;
+      }
+
+      if (status === PLAYER_STATE.PAUSED) {
+        console.log('일시정지');
+        if (displayLogDebounce) {
+          // window.clearTimeout(displayLogDebounce);
+        }
+        displayLogo = true;
+        return;
+      }
+
+      if (status === PLAYER_STATE.BUFFERING) {
+        console.log('버퍼');
+        // setTimeout(() => {
+        //   firstLoad = false;
+        // }, 4000);
+        return;
+      }
+    });
+  }
+
+  function pause() {
+    clearTimeout(pauseTimer);
+    pauseTimer = setTimeout(() => {
+      player.pauseVideo();
+    }, 1000);
+  }
+
+  function play() {
+    player.playVideo();
+  }
+
+  onMount(async () => {
+    loadYoutubePlayer();
+    onPlayerReady();
+    onPlayerStateChange();
+
+    setInterval(() => {
+      playTime = player.getCurrentTime();
+    }, 1000);
+  });
+</script>
+<div class="player-wrap" on:click={onClick}>
+    <div id='{playerId}' class="youtube-player"></div>
+    <div class="overlay-wrap">
+        {#if player}
+            <div class="running-time overlay">
+                {#await playTime}
+                    ...waiting
+                {:then number}
+                    {toHHMMSS(number)}
+                {:catch error}
+                    {error.message}
+                {/await}
+            </div>
+        {/if}
+    </div>
+</div>
+
+
+<style>
+    .player-wrap {
+        display: inline-block;
+        width: 100%;
+        height: auto;
+        padding-top: 56.25%;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .player-wrap .youtube-player {
+        z-index: 1;
+        top: 0;
+        left: 0;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
+    }
+
+    .player-wrap .overlay {
+        position: absolute;
+    }
+
+    .player-wrap .overlay-wrap {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 2;
+    }
+
+    .player-wrap .hide {
+        opacity: 0;
+        transition: opacity 0.3s;
+    }
+
+    .player-wrap .running-time {
+        bottom: 15px;
+        right: 16px;
+        z-index: 3;
+        display: flex;
+        width: 110px;
+        height: 26px;
+        background: #323232;
+        border-radius: 5px;
+        color: #fff;
+        align-items: center;
+        justify-content: center;
+        font-family: system-ui;
+    }
+
+    @media (max-width: 495px) {
+        .player-wrap .running-time {
+            width: 85px;
+            height: 20px;
+            font-size: 12px;
+        }
+    }
 </style>
