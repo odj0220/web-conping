bd70278e43fb50770a488c44d415664671b4c29b
Charlie <ickyicky@gmail.com>
2022-07-07 16:32:55 +0900

    시리즈 보러가기 라우팅 추가

--- src/components/SeriesVideoContainer.svelte
+++ src/components/SeriesVideoContainer.svelte
@@ -1,35 +1,42 @@
 <script>
-import CenterSection from '../styles/CenterSection.svelte';
-import GridViewList from './GridViewList.svelte';
-import Button from './Button.svelte';
-import { onMount } from 'svelte';
-import Title from './Title.svelte';
+  import { onMount } from 'svelte';
+  
+  import { goto } from '$app/navigation';
 
-const handleClick = (id) => {
-  console.log('TODO: click 이벤트', id);
-};
+  import CenterSection from '../styles/CenterSection.svelte';
+  import GridViewList from './GridViewList.svelte';
+  import Button from './Button.svelte';
+  import Title from './Title.svelte';
 
-let contents = [];
-let seriesId;
-let title = [];
-
-onMount(async () => {
-  const body = {
-    query: '{getMainSeries{title {text type} series {name id} contents {thumb name videoId program {name}}}}',
+  const handleItemClick = (id) => {
+    console.log('TODO: click 이벤트', id);
   };
-  fetch('/api/graphql', {
-    method: 'POST',
-    body: JSON.stringify(body),
-  }).then(response => {
-    return response.json();
-  }).then(response => {
-    const { title: titleArray, series, contents: contentList } = response.data.getMainSeries;
-    title = titleArray;
-    seriesId = series.id;
-    contents = contentList;
-  });
-});
 
+  const handleButtonClick = () => {
+    console.log('TODO: click 이벤트');
+    goto(`/programs/${contents.programId}`);
+  };
+
+  let contents = [];
+  let title = [];
+  let seriesId;
+
+  onMount(async () => {
+    const body = {
+      query: '{getMainSeries{title {text type} series {name id} contents {thumb name videoId programId program {name}}}}',
+    };
+    fetch('/api/graphql', {
+      method: 'POST',
+      body: JSON.stringify(body),
+    }).then(response => {
+      return response.json();
+    }).then(response => {
+      const { title: titleArray, series, contents: contentList } = response.data.getMainSeries;
+      title = titleArray;
+      seriesId = series.id;
+      contents = contentList;
+    });
+  });
 </script>
 
 <CenterSection>
@@ -37,11 +44,12 @@ onMount(async () => {
   
   <GridViewList
     {contents}
-    onClick={handleClick}
+    onClick={handleItemClick}
   />
 
+  <!-- TODO: 버튼명 하드코딩 수정 -->
   <Button
     buttonName={'랜선뷰티 시리즈 보러가기'}
-    onClick={handleClick}
+    onClick={handleButtonClick}
   />
 </CenterSection>
