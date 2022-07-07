1dbc0ca9edec88fd5b7b78a8d910c5f20aa741af
Charlie <ickyicky@gmail.com>
2022-07-07 12:08:41 +0900

    프로그램 상세 페이지 생성

---
+++ src/routes/programs/[id].svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+  
+  import ProgramListContainer from '$component/ProgramListContainer.svelte';
+
+  const id = $page.params.id;
+</script>
+
+<!-- <HeaderContainer /> TODO: 헤더 -->
+
+
+<ProgramListContainer {id} />
