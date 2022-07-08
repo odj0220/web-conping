8434d368c026d4711007f1751555ec0f73372cff
Charlie <ickyicky@gmail.com>
2022-07-08 15:00:37 +0900

    GlobalBlock 추가

---
+++ src/styles/GlobalBlock.svelte
@@ -0,0 +1,14 @@
+<main>
+  <slot />
+</main>
+
+<style lang="scss">
+  @import '../styles/modules.scss';
+  @import '../styles/variables.scss';
+
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+
+</style>
