7864404502ea5e3648e0c5933c90039ddc41ba54
cheonyulin@gmail.com <mason@gollala.com>
2022-07-08 14:37:03 +0900

    create and publish PastTimeDelta.svelte

---
+++ src/components/PastTimeDelta.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import TimeAgo from 'javascript-time-ago';
+    import ko from 'javascript-time-ago/locale/ko';
+
+    TimeAgo.addDefaultLocale(ko);
+
+    export let pastTime = 0;
+    export let style: any;
+    export let className: string;
+    const timeSystem = new TimeAgo('ko');
+
+    $: timeDelta = timeSystem.format(pastTime);
+</script>
+
+<span {style} class={className}>
+    {timeDelta}
+</span>
+
+<style lang="scss">
+
+</style>
