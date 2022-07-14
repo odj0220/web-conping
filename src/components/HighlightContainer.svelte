2664917da3a2f21cf1f86801cd5821fe94fc9146
cheonyulin@gmail.com <mason@gollala.com>
2022-07-14 18:36:17 +0900

    update to fetch data in HighLightContainer

--- src/components/HighlightContainer.svelte
+++ src/components/HighlightContainer.svelte
@@ -3,19 +3,19 @@
   import PreviewVideos from './PreviewVideos.svelte';
   
   export let id;
-  let contents: {data: any[]; end: boolean; cursor:string} = {
-    data: [],
-    end: false,
-    cursor: '',
-  };
   
   const handleClick = (id) => {
     console.log('TODO: click 이벤트', id);
   };
 
   async function loadHighlight() {
-    const query = `{getContentsByProgramId(id:"${id}", type:HIGHLIGHT){id name programId videoId thumb createDt round program {id name}}}`;
+    const query = `{getContentsByProgramId(id:"${id}", type:HIGHLIGHT){id title programId videoId thumb createDt episode program {id title}}}`;
     const result = await graphqlApi(query);
+    const contents: {data: any[]; end: boolean; cursor:string} = {
+      data: [],
+      end: false,
+      cursor: '',
+    };
     contents.data = result.data.getContentsByProgramId;
     return contents;
   }
@@ -24,10 +24,14 @@
 {#await loadHighlight()}
     <p>loading...</p>
 {:then data}
-    <PreviewVideos
-            contents={data}
-            onClick={handleClick}
-    />
+    {#if data.length}
+        <PreviewVideos
+                contents={data}
+                onClick={handleClick}
+        />
+    {:else}
+        <p>하이라이트가 없습니다.</p>
+    {/if}
 {/await}
 
 <style lang="scss">
