bc2a0d27996b015b1d4a2924ff7de12570312d33
cheonyulin@gmail.com <mason@gollala.com>
2022-07-14 18:09:21 +0900

    programDetail 영역까지 데이터 처리 완료

--- src/components/Metadata.svelte
+++ src/components/Metadata.svelte
@@ -23,7 +23,7 @@ export let option: {
           airingBeginAt?: number;
           airingEndAt?: number;
           totalEpisode?: number;
-          airingAt?: number;
+          regularAiringAt?: number;
         };
     }
 
@@ -45,17 +45,10 @@ let pastTime: number;
 $: isContentDetail = !!option['contentDetail'];
 $: isProgramDetail = !!option['programDetail'];
 
-onMount(async () => {
-    getInformation();
-    setTimeout(() => {
-      console.log(option['contentDetail']);
-    }, 1000);
-});
-
 const getInformation = async () => {
+    let result = '';
     if (isContentDetail) {
       const { contentDetail: { info: { programTitle, episode, createDt, view } } } = option;
-      let result = '';
 
       if (programTitle) {
         information = [programTitle];
@@ -81,15 +74,87 @@ const getInformation = async () => {
     }
 
     if (isProgramDetail) {
-      return '프로그램 정보';
+      const { programDetail: { info: { airingBeginAt, airingEndAt, totalEpisode, regularAiringAt } } } = option;
+  
+      if (airingBeginAt) {
+        const start = convertDate(airingBeginAt) + ' ~';
+        let end = '';
+        if (airingEndAt) {
+          end = convertDate(airingEndAt);
+        }
+
+        information = [...information, start + end];
+      }
+
+      if (totalEpisode) {
+        information = [...information, `${totalEpisode}부작`];
+      }
+
+      if (regularAiringAt) {
+        const regularTime = convertRegularDate(regularAiringAt);
+        information = [...information, regularTime];
+      }
+
+      result = information.join(' ・ ');
+
+      return result;
     }
 
     return '';
 };
+
+const convertDate = (time: number) => {
+    const date = new Date(time);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}.${month < 10 ? `0${month}` : month}.${day < 10 ? `0${day}` : day}`;
+};
+
+const convertRegularDate = (time:number) => {
+    const date = new Date(time);
+    const day = getDay(date.getDay());
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const division = hours > 12 ? '오후' : '오전';
+    return `(${day}) ${division} ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+};
+
+const getDay = (time: number) => {
+    const DAYS = {
+      SUNDAY: 0,
+      MONDAY: 1,
+      TUESDAY: 2,
+      WENSDAY: 3,
+      THURSDAY: 4,
+      FRIDAY: 5,
+      SATURDAY: 6,
+    };
+
+    switch (time) {
+    case DAYS.SUNDAY:
+      return '일';
+    case DAYS.MONDAY:
+      return '월';
+    case DAYS.TUESDAY:
+      return '화';
+    case DAYS.WENSDAY:
+      return '수';
+    case DAYS.THURSDAY:
+      return '목';
+    case DAYS.FRIDAY:
+      return '금';
+    case DAYS.SATURDAY:
+      return '토';
+    default:
+      return '알 수 없음';
+    }
+};
 </script>
 
 <div class="container">
-  {#if isContentDetail}
+    {#if isContentDetail}
       {#await getInformation()}
       {:then text}
           <div class="info">
@@ -125,46 +190,50 @@ const getInformation = async () => {
               </GrayBox>
           </section>
       {/if}
-  {/if}
+    {/if}
+
+
+    {#if isProgramDetail}
+        {#if option['programDetail']['title']}
+            <h4 class="name">
+                {option['programDetail']['title']}
+            </h4>
+        {/if}
+        {#await getInformation()}
+        {:then text}
+            <div class="info">
+                {text}
+            </div>
+        {/await}
+        {#if option['programDetail']['description']}
+            <div class="description">
+                {option['programDetail']['description']}
+            </div>
+        {/if}
+
+        {#if option.programDetail?.celebs.length}
+            <section class="celob-wrapper">
+                <GrayBox title="출연 셀럽">
+                    <Hscroller>
+                        <ul class="profile-list">
+                            {#each option.programDetail.celebs as celeb, index}
+                                <li class="profile-item">
+                                    <a sveltekit:prefetch href={'/celobs/' + celeb.id}>
+                                        <Avatar size="8rem" src={celeb.thumbnail} alt="{celeb.name + '의 사진'}"/>
+                                        <span class="profile-name">{celeb.name}</span>
+                                        <span class="profile-role">{celeb.categories[0]}</span>
+                                    </a>
+                                </li>
+                            {/each}
+                        </ul>
+                    </Hscroller>
+                </GrayBox>
+            </section>
+        {/if}
+    {/if}
 </div>
-<!--<section class="container">-->
-<!--    {#if programTitle}-->
-<!--        <h4 class="name">{programTitle}</h4>-->
-<!--    {/if}-->
-<!--    <span class="info">-->
-<!--      {#if airingBeginAt && airingEndAt}-->
-<!--          {airingBeginAt}~{airingEndAt} &middot-->
-<!--      {/if}-->
-<!--      8부작 &middot-->
-<!--      {airingAt}-->
-<!--    </span>-->
 
-<!--    {#if contentTitle}-->
-<!--        <h5 class="title">{contentTitle}</h5>-->
-<!--    {/if}-->
-
-<!--    {#if description}-->
-<!--        <div class="description">{description}</div>-->
-<!--    {/if}-->
-
-<!--    {#if celebs?.length}-->
-<!--      <GrayBox title="골라라 셀럽">-->
-<!--        <Hscroller>-->
-<!--          <ul class="profile-list">-->
-<!--            {#each celebs as celeb}-->
-<!--              <li class="profile-item">-->
-<!--                <Avatar size="80px" src="{celeb.thumbnail}" />-->
-<!--                <span class="profile-name">{celeb.name}</span>-->
-<!--              </li>-->
-<!--            {/each}-->
-<!--          </ul>-->
-<!--        </Hscroller>-->
-<!--      </GrayBox>-->
-<!--    {/if}-->
-<!--  </section>  -->
-
-
-  <style lang="scss">
+<style lang="scss">
     .container {
       padding: 1.6rem;
       .content-name {
@@ -225,4 +294,4 @@ const getInformation = async () => {
         }
       }
     }
-  </style>
+</style>
