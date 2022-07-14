b8ea36747cd5d6107d07b83b33a55fd7fbb2cc15
cheonyulin@gmail.com <mason@gollala.com>
2022-07-14 18:35:59 +0900

    update to fetch data in EpisodeContainer

--- src/components/EpisodeContainer.svelte
+++ src/components/EpisodeContainer.svelte
@@ -1,26 +1,32 @@
 <script lang=ts>
-  import { onMount } from 'svelte';
-  
   import { graphqlApi } from '../lib/_api_graphql';
-  
   import ImageListView from './ImageListView.svelte';
 
   export let id;
 
-  let contents = [];
 
-  onMount(async () => {
-    const query = `{getContentsByProgramId(id:"${id}", type:EPISODE){id name videoId thumb program {name}}}`;
+  async function loadContents() {
+    const query = `{getContentsByProgramId(id:"${id}", type:FULL){id title videoId thumb program {title}}}`;
     const result = await graphqlApi(query);
-    contents = result.data.getContentsByProgramId;
-  });
+    const contents = result.data.getContentsByProgramId;
+    return contents;
+  }
 
   const handleClick = (clickedId: string) => {
     console.log('TODO: click 이벤트', clickedId);
   };
 </script>
 
-<ImageListView
-  {contents}
-  onClick={handleClick}
-/>
+{#await loadContents()}
+{:then contents}
+    {#if contents.length}
+        {contents.length}
+        <ImageListView
+                {contents}
+                onClick={handleClick}
+        />
+    {:else}
+        <p>에피소드가 없습니다.</p>
+    {/if}
+{/await}
+
