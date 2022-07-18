5f156820ae517326df40326a996a0c2371da2350
Mihyang Seo <mihyangseo@Mihyangui-MacBookPro.local>
2022-07-18 18:06:42 +0900

    쇼핑존 메인 정렬옵션 작업중

--- src/components/common/shared/DimmendSorter.svelte
+++ src/components/common/shared/DimmendSorter.svelte
@@ -6,4 +6,31 @@ import Dimmend from '../layout/Dimmend.svelte';
   <svelte:fragment slot="header">
     정렬 기준
   </svelte:fragment>
+  <svelte:fragment slot="body">
+    정렬 기준
+  </svelte:fragment>
+  <svelte:fragment slot="footer">
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+    fdgsdfgsdfgsfd<br />
+  </svelte:fragment>
 </Dimmend>
