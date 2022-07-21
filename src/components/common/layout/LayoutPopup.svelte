31a28f2a392d9edb5fa8d2e52994d1ccd283dd3f
Charlie <ickyicky@gmail.com>
2022-07-21 15:21:37 +0900

    컴포넌트 위치 수정

---
+++ src/components/common/layout/LayoutPopup.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+
+  export let visible = false;
+</script>
+
+{#if visible}
+  <div
+    class="wall"
+    out:fade
+  >
+    <slot />
+  </div>
+{/if}
+
+<style lang="scss">
+  .wall {
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.66);
+  }
+</style>
