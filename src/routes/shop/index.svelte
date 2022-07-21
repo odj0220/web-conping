e7b8f7ff8d501e35871325c99a7a95721e984756
Charlie <ickyicky@gmail.com>
2022-07-21 15:20:55 +0900

    header 위치 수정

--- src/routes/shop/index.svelte
+++ src/routes/shop/index.svelte
@@ -1,5 +1,8 @@
 <script>
+  import MainHeaderContainer from '$container/MainHeaderContainer.svelte';
+
   import ShopContainer from '$container/ShopContainer.svelte';
 </script>
 
+<MainHeaderContainer title="쇼핑존" />
 <ShopContainer />
