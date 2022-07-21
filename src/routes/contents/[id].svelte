1aebe84446814fa83e2c969d94ccb3f772d7da67
cheonyulin@gmail.com <mason@gollala.com>
2022-07-21 10:10:13 +0900

    re-render when changed id

--- src/routes/contents/[id].svelte
+++ src/routes/contents/[id].svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import { page } from '$app/stores';
+
   import ContentsDetailContainer from '$container/ContentsDetailContainer.svelte';
-  import { beforeNavigate } from '$app/navigation';
 
-  const id = $page.params.id;
-
-  beforeNavigate((navigation) => {
-    const { to } = navigation;
-    window.location.href = to.pathname;
-  });
+  $: id = $page.params.id;
 </script>
 
-<ContentsDetailContainer {id} />
+{#key id}
+    <ContentsDetailContainer id={id} />
+{/key}
