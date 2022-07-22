e81cc83e8c2a6fbe66f1910847a1d2b944fb6d5e
cheonyulin@gmail.com <mason@gollala.com>
2022-07-22 17:05:47 +0900

    save icon

---
+++ src/components/Footer.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import Icon from './icons/Icon.svelte';
+</script>
+
+<footer>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">
+                    <Icon name="gnb_home"/>
+                </a>
+            </li>
+
+            <li>
+                <a href="/">
+                    <Icon name="gnb_shop"/>
+                </a>
+            </li>
+
+            <li>
+                <a href="/">
+                    <Icon name="gnb_celeb"/>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</footer>
+
+<style lang="scss">
+    footer {
+      width: 100%;
+      position: sticky;
+      bottom: 0;
+      left: 0;
+      nav {
+        width: 100%;
+        ul {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          li {
+            width: 100%;
+          }
+        }
+      }
+    }
+
+</style>
