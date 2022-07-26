5a4c7aad5191160d0d416d144ca2724db772c6c3
Charlie <ickyicky@gmail.com>
2022-07-26 13:40:26 +0900

    카테고리 스크롤 위치 동적 변경

--- src/components/Avatar.svelte
+++ src/components/Avatar.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
-    export let size = '';
-    export let src = '';
-    export let alt = '';
+  export let size = '';
+  export let src = '';
+  export let alt = '';
 </script>
 
 <div class="container" style="width: {size}; height: {size}">
-    <img src={src}  alt={alt}/>
+  <img src={src} alt={alt}/>
 </div>
 
 <style lang="scss">
-    .container {
-        border-radius: 50%;
-        overflow: hidden;
-        flex-shrink: 0;
-        background-color: gray;
-        img {
-            width: 100%;
-            height: 100%;
-        }
+  .container {
+    border-radius: 50%;
+    overflow: hidden;
+    flex-shrink: 0;
+    background-color: gray;
+    
+    img {
+      width: 100%;
+      height: 100%;
     }
+  }
 </style>
