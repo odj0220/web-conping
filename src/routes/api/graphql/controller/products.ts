73ce48d1552c5b0f1c8a8e313744ab87d50e6f30
cheonyulin@gmail.com <mason@gollala.com>
2022-07-26 15:19:41 +0900

    update products contents

--- src/routes/api/graphql/controller/products.ts
+++ src/routes/api/graphql/controller/products.ts
@@ -1,29 +1,49 @@
 import productJson from '../../../../../static/data/product.json';
 import relationJson from '../../../../../static/data/relation.json';
-import celebJson from '../../../../../static/data/celeb.json';
-import contentsJson from '../../../../../static/data/content.json';
 import type { Product, VideoContent } from '$lib/models/backend/backend';
 import type { IProduct } from '../../../../global/types';
 import { GET } from '$lib/_api';
 import { convertProduct } from './util';
-import { getParams } from '../../../../lib/utils/common';
 import { getThumbnail } from './util';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const products = async ({ order, category }: {order: string, category: string}) => {
-  const data: any[] = productJson;
+export const products = async ({ order, category, limit, page }: {order: 'latest' | 'popularity' | 'highPrice' | 'lowPrice' | 'alphabetical', category: number, limit:number, page: number}) => {
+  let sort: any[] = [{ 'views': 'asc' }];
 
-  const query: any = {
-    sort: JSON.stringify([{ 'views': 'asc' }]),
+  switch (order) {
+  case 'latest':
+    sort = [{ 'createdAt': 'asc' }];
+    break;
+  case 'popularity':
+    sort = [{ 'views': 'asc' }];
+    break;
+  case 'highPrice':
+    sort = [{ 'price': 'desc' }];
+    break;
+  case 'lowPrice':
+    sort = [{ 'price': 'asc' }];
+    break;
+  case 'alphabetical':
+    sort = [{ 'name': 'asc' }];
+    break;
+  }
+
+  const params:any = {
+    sort: JSON.stringify(sort),
     videoContent: true,
     celeb: true,
-    size: 2,
+    size: limit || 10,
+    page: page || 1,
   };
 
-  const products = await getProductsFromBackend(query);
+  if (category) {
+    params['category'] = category;
+  }
 
-  return products.elements.map((product: any) => {
+
+  const response: any = await GET('/product', { method: 'GET', params: params });
+  const products = response.elements.map((product: any) => {
     const relatedItems = [];
     /* Generate Contents */
     const contents = product.VideoContentProduct.map((p: any) => {
@@ -63,7 +83,7 @@ export const products = async ({ order, category }: {order: string, category: st
       });
     }
 
-    return {
+    const node = {
       id: product.id,
       name: product.name,
       brand: 'No Brand',
@@ -77,69 +97,24 @@ export const products = async ({ order, category }: {order: string, category: st
       celebs,
       relatedItems,
     };
+
+    const cursor = product.id;
+
+    return {
+      node,
+      cursor,
+    };
   });
 
-  // data = data.map((product) => {
-  //   const relations: any[] = relationJson.filter((r) => r.product === product.id);
-  //   const celebs: any[] = celebJson.filter((celeb) => relations.find((r) => r.celeb === celeb.id));
-  //   const contents: any[] = contentsJson.filter((content) => relations.find((r) => r.content === content.id));
-  //
-  //   return {
-  //     ...product,
-  //     celebs,
-  //     contents,
-  //     relatedItems: [
-  //       {
-  //         thumbnail: celebs[0].thumbnail,
-  //         title: celebs[0].name,
-  //         type: 'Celeb',
-  //         id: celebs[0].id,
-  //       },
-  //       {
-  //         thumbnail: contents[0].thumb,
-  //         title: contents[0].title,
-  //         type: 'Content',
-  //         id: contents[0].id,
-  //       },
-  //     ],
-  //   };
-  // });
-  //
-  //
-  // if (category) {
-  //   data = data.filter((product) => product.category === category);
-  // }
-  //
-  // if (order) {
-  //   switch (order) {
-  //   case 'latest':
-  //     data.sort((dataA, dataB) => dataB.createDt - dataA.createDt);
-  //     break;
-  //   case 'popularity':
-  //     data.sort((dataA, dataB) => dataB.views - dataA.views);
-  //     break;
-  //   case 'highPrice':
-  //     data.sort((dataA, dataB) => dataB.price - dataA.price);
-  //     break;
-  //   case 'lowPrice':
-  //     data.sort((dataA, dataB) => dataA.price - dataB.price);
-  //     break;
-  //   case 'alphabetical':
-  //     data.sort((dataA, dataB) => {
-  //       if (dataA.name > dataB.name) {
-  //         return -1;
-  //       } else if (dataB.name > dataA.name) {
-  //         return 1;
-  //       } else {
-  //         return 0;
-  //       }
-  //     });
-  //     break;
-  //   default:
-  //     data.sort((dataA, dataB) => dataB.createDt - dataA.createDt);
-  //     break;
-  //   }
-  // }
+  return {
+    totalCount: response.totalElements,
+    edges: products,
+    pageInfo: {
+      page: response.page,
+      totalPage: response.totalPages,
+      hasNextPage: response.page < response.totalPages,
+    },
+  };
 };
 
 export const product = async ({ id }: {id: string}) => {
@@ -172,21 +147,3 @@ export const getProductByCelebId = ({ id, limit }: { id: string, limit: number }
   }
   return products;
 };
-
-async function getProductsFromBackend(params?: {size?: number; page?: number; sort?: string; videoContent?: boolean; productCategoryId: number; celeb: boolean}) {
-  let queries = '';
-
-  if (params) {
-    queries = getParams(params);
-  }
-
-  const response = await GET(`/product${queries ? `?${queries}` : ''}`);
-  return response;
-
-  // if (response.ok) {
-  //   const products = await response.json();
-  //   return products;
-  // }
-  //
-  // throw { message: 'Something went wrong' };
-}
