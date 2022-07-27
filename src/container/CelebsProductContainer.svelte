34f7f2105c6b98d5fb1fe2b5bb997b43aa2abc86
cheonyulin@gmail.com <mason@gollala.com>
2022-07-27 21:11:15 +0900

    셀럽 상세 페이지 무한스크롤 이용해서 상품 불러오는 기능 추가

--- src/container/CelebsProductContainer.svelte
+++ src/container/CelebsProductContainer.svelte
@@ -6,8 +6,6 @@
   import ProductGridList from '$component/ProductGridList.svelte';
   import Title from '$component/Title.svelte';
   import MoreButton from '$component/common/shared/MoreButton.svelte';
-  import { onMount } from 'svelte';
-
   import type { IProduct, TitleElement } from 'src/global/types';
 
   export let title : TitleElement[] = [];
@@ -51,32 +49,39 @@
     let cursor = getProductsByCelebId.pageInfo.startCursor;
   };
 
-  onMount(async () => {
-    getData();
-  });
-
+  async function runInfiniteScrolling(event) {
+    const detail = event.detail;
+    await getData();
+    detail.stop();
+  }
 </script>
 
 {#await getData()}
 {:then}
   {#if products.length}
-    <Container margin="5.6rem 0 0">
-      {#if title.length}
-        <Title title={title}/>
-        <ProductGridList data={[...products].slice(0, 4)}/>
+      <Container margin="5.6rem 0 0">
+        {#if title.length}
+            <Title title={title}/>
+            <ProductGridList data={products}/>
+            {#if moreButton && products.length >= 4}
+              <MoreButton value={`${category} 상품 더보기`}/>
+            {/if}
         {:else}
-        <ProductGridList {products}/>
-      {/if}
-
-      {#if moreButton && products.length > 4}
-        <MoreButton value={`${category} 상품 더보기`}/>
-      {/if}
-    </Container>
+          <ProductGridList
+                  data={products}
+                  {infiniteScroll}
+                  {end}
+                  {cursor}
+                  on:request-more={runInfiniteScrolling}
+          />
+        {/if}
+      </Container>
     {:else}
-    {#if title.length}
-
-    {:else}
-      <EmptyMessage text={`${category}님의 상품`} />
-    {/if}
+      {#if !title.length}
+        <EmptyMessage text={`${category}님의 상품`} />
+      {/if}
   {/if}
 {/await}
+
+<style lang="scss">
+</style>
