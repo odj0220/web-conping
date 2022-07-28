47c2568f38274b232107675f4cac256299398dc7
dj <dj@gollala.com>
2022-07-28 14:08:06 +0900

    사용하지 않는 import 제거

--- src/routes/contents/[id].svelte
+++ src/routes/contents/[id].svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { page } from '$app/stores';
-
   import ContentsDetailContainer from '$container/ContentsDetailContainer.svelte';
-  import { onMount } from 'svelte';
 
   $: id = $page.params.id;
 </script>
